01a9f96eea668cce91de09a9dde03d0a30b5665a
MatiasWod <mwodtke@itba.edu.ar>
2023-04-01 20:26:49 -0300

    Added .gitignore and modified md5sum.c

--- md5sum.c
+++ md5sum.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define BUFF_LEN 256
 #define SLAVES_QTY 4
@@ -28,7 +29,7 @@ int main(int argc, char *argv[])
     char **paths = calloc(argc - 1, sizeof(char *));
     if (paths == NULL)
     {
-        char errmsg[] = "Failed to allocate memory for paths\n";
+        char errmsg[] = "Failed to allocate memory for paths";
         print_error_msg(errmsg);
     }
 
@@ -51,42 +52,46 @@ int main(int argc, char *argv[])
 
 void create_slave_processes(char **paths, int paths_qty)
 {
+    // Slave restriction and identifiers
     int n_slave;
+    int max_slaves = (SLAVES_QTY < (paths_qty / 2)) ? SLAVES_QTY : (paths_qty / 2);
+    int pipefd[max_slaves][2]; // fd para los pipes que vamos a crear
     int files_remaining = paths_qty;
-    int pipefd[SLAVES_QTY][2]; // fd para los pipes que vamos a crear
 
     char *newargv[] = {"slave", NULL};
     char *newenv[] = {NULL};
 
     // Create all pipes nescesary for the comunication between md5sum and slaves
-    for (n_slave = 0; n_slave < SLAVES_QTY; n_slave++)
+    for (n_slave = 0; n_slave < max_slaves; n_slave++)
     {
         if (pipe(pipefd[n_slave]) != 0)
         {
-            perror("pipe");
-            exit(1);
+            char errmsg[] = "Failed to create pipe";
+            print_error_msg(errmsg);
         }
-    }
 
-    for (n_slave = 1; (n_slave <= paths_qty / 2) && (n_slave <= SLAVES_QTY); n_slave++)
-    {
         if (fork() == 0) // Child process
         {
-            close(pipefd[n_slave - 1][WRITE]);
-            dup2(pipefd[n_slave - 1][READ], STDIN_FILENO);
-            close(pipefd[n_slave - 1][READ]);
+            close(pipefd[n_slave][WRITE]);
+            dup2(pipefd[n_slave][READ], STDIN_FILENO);
+            close(pipefd[n_slave][READ]);
 
             execve("slave", newargv, newenv);
         }
-        else // Parent process
+    }
+
+    // Send files to slave processes
+    // TODO RECIBE 2 FILES PERO PROCESA SOLO UNO PORQUE ACA MAX SLAVES ES 1 (ARREGLAR)
+    for (int slave = 0, file = 0; slave < max_slaves && files_remaining > 0; file++, slave++, files_remaining--)
+    {
+
+        close(pipefd[slave][READ]); // Close the read end of the pipe in the parent process
+        if (write(pipefd[slave][WRITE], paths[file], strlen(paths[file])) == -1)
         {
-            close(pipefd[n_slave - 1][READ]); // Close the read end of the pipe in the parent process
-
-            char *message = "Perfect muwu :3\0";
-            write(pipefd[n_slave - 1][WRITE], message, strlen(message));
-
-            close(pipefd[n_slave - 1][WRITE]);
+            char errmsg[] = "Failed to send paths to slave process";
+            print_error_msg(errmsg);
         }
+        close(pipefd[slave][WRITE]);
     }
 }
 
