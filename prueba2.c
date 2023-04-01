8f458a7c65c3e4ffa34a19b6092742c1b03dea12
bcanevaro <bcanevaro@itba.edu.ar>
2023-04-01 18:09:04 -0300

    Added md5 process to slave.

---
+++ prueba2.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+    printf("pepe");
+    return 0;
+}
--- slave.c
+++ slave.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <openssl/md5.h>
 
 int main()
 {
-    printf("%d\n", getpid());
+    unsigned char c[MD5_DIGEST_LENGTH];
+    char *filename="prueba2.c";
+    int i;
+    FILE *inFile = fopen (filename, "rb");//rb for non text files
+    MD5_CTX mdContext;
+    int bytes;
+    unsigned char data[1024];
+
+    if (inFile == NULL) {
+        printf ("%s can't be opened.\n", filename);
+        return 0;
+    }
+
+    MD5_Init (&mdContext);
+    while ((bytes = fread (data, 1, 1024, inFile)) != 0)
+        MD5_Update (&mdContext, data, bytes);
+    MD5_Final (c,&mdContext);
+    printf("PID:%d ",getpid());
+    for(i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", c[i]);
+    printf (" %s\n", filename);
+    fclose (inFile);
+    return 0;
 }
