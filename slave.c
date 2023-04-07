8cc4e81d19ec2c1308bce58f834bd1bee4edfd87
MatiasWod <mwodtke@itba.edu.ar>
2023-04-07 17:14:25 -0300

    Changed slave.c

--- slave.c
+++ slave.c
@@ -1,47 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-// #include <openssl/md5.h>
-#define BUF_SIZE 50
-
+#define BUF_SIZE 1024
 int main()
 {
     char buffer[BUF_SIZE];
-
-    read(STDIN_FILENO, buffer, BUF_SIZE);
-
-    int fp = open("out.txt", O_RDWR | O_CREAT, 0644);
-
-    if (fp == -1)
+    while (scanf("%s", buffer) != EOF)
     {
-        perror("Error opening file");
-        return 1;
+        execlp("md5sum", "md5sum", buffer, NULL);
     }
-
-    write(fp, buffer, BUF_SIZE);
-    close(fp);
     return 0;
 }
-/*
-unsigned char c[MD5_DIGEST_LENGTH];
-char *filename="prueba2.c";
-int i;
-FILE *inFile = fopen (filename, "rb");//rb for non text files
-MD5_CTX mdContext;
-int bytes;
-unsigned char data[1024];
-
-if (inFile == NULL) {
-    printf ("%s can't be opened.\n", filename);
-    return 0;
-}
-
-MD5_Init (&mdContext);
-while ((bytes = fread (data, 1, 1024, inFile)) != 0)
-    MD5_Update (&mdContext, data, bytes);
-MD5_Final (c,&mdContext);
-printf("PID:%d ",getpid());
-for(i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", c[i]);
-printf (" %s\n", filename);
-fclose (inFile);
-return 0;*/
