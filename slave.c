f998222d98ae939bf936725e1704359cce47f1b3
MatiasWod <mwodtke@itba.edu.ar>
2023-04-11 13:35:46 -0300

    slave.c now reads correctly extensions of files

--- slave.c
+++ slave.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,16 +12,15 @@ int main()
 {
     char *buffer = NULL;
     size_t bufsize = 0;
-    ssize_t bytes_readed;
 
     int pid = (int)getpid();
     int i = 0;
 
-    while (bytes_readed = getline(&buffer, &bufsize, stdin) != EOF)
+    while (getline(&buffer, &bufsize, stdin) != EOF)
     {
-        buffer[bytes_readed + 1] = '\0';
+        buffer[strlen(buffer) - 1] = '\0';
         int fp = open("outa.txt", O_WRONLY | O_APPEND | O_CREAT, 0644);
-        dprintf(fp, "%d\t%s\tbytes_read = %d\n", pid, buffer, (int)bytes_readed);
+        dprintf(fp, "%d\t%s\tbytes_read = %d\n", pid, buffer, strlen(buffer));
         close(fp);
 
         if (fork() == 0)
