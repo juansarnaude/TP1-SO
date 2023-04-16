66a750c8c4e5dd05acb4cb4eea425204fea0fb80
MatiasWod <mwodtke@itba.edu.ar>
2023-04-16 12:49:59 -0300

    Perfected slave.c

---
+++ lib/slave.h
@@ -0,0 +1,5 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
--- slave.c
+++ slave.c
@@ -1,13 +1,7 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "./lib/slave.h"
 
-#define BUF_SIZE 1024
 int main()
 {
     char *buffer = NULL;
@@ -18,15 +12,10 @@ int main()
     while (getline(&buffer, &bufsize, stdin) != EOF)
     {
         buffer[strlen(buffer) - 1] = '\0';
-        int fp = open("outa.txt", O_WRONLY | O_APPEND | O_CREAT, 0644);
-        dprintf(fp, "%d\t%s\tbytes_read = %d\n", pid, buffer, strlen(buffer));
-        close(fp);
-
         if (fork() == 0)
         {
             execlp("md5sum", "md5sum", buffer, NULL);
         }
-        wait(NULL);
     }
     return 0;
 }
